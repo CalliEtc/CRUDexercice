839d204f1bdbe428414fc88709cb2f6574995763
CalliEtc <callipter@gmail.com>
2024-06-23 17:48:16 +0200

    Hash password, JWT token end JWT verify add to the exercice and it worked

--- app.js
+++ app.js
@@ -1,120 +1,126 @@
-// Importer le module Express pour créer une application web
+// Importation du module Express pour créer une application web
 const express = require('express');
 
-// Créer une application Express
+// Création d'une application Express
 const app = express();
 
-// Utiliser le middleware express.json() pour parser les corps de requête en JSON
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+// Utilisation du middleware express.json() pour analyser les corps de requête en JSON
 app.use(express.json());
 
-// Définir un tableau d'objets pour stocker les utilisateurs
+// Définition d'un tableau d'objets pour stocker les utilisateurs
 let users = [
-    { id: 1, name: 'Alice', email: 'alice@example.com' },
-    { id: 2, name: 'Bob', email: 'bob@example.com' },
-    { id: 3, name: 'Charlie', email: 'charlie@example.com' }
+    { id: 1, name: 'Alice', email: 'alice@example.com', password: 'abcde' },
+    { id: 2, name: 'Bob', email: 'bob@example.com', password: '123456' },
+    { id: 3, name: 'Charlie', email: 'charlie@example.com', password: 'azerty' }
 ];
 
+// Middleware pour hacher le mot de passe
+const hashPassword = async (req, res, next) => {
+    argon2.hash(req.body.password)
+        .then((hashedPassword) => {
+            console.log("asmaa toujours plus forte que moi");
+            req.body.password = hashedPassword; // Remplacer le mot de passe par le mot de passe haché
+
+            next();
+        })
+        .catch((err) => {
+            console.error(err); // Afficher l'erreur dans la console
+            res.sendStatus(500); // Répondre avec une erreur serveur
+        });
+};
+
+// Middleware pour vérifier le token
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']; // Extraire l'en-tête authorization
+    const token = authHeader && authHeader.split(' ')[1]; // Extraire le token de l'en-tête
+
+    if (!token) return res.sendStatus(401); // Si pas de token, renvoyer une réponse 401 (Non autorisé)
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403); // Si le token est invalide, renvoyer une réponse 403 (Interdit)
+        req.user = user; // Ajouter les informations de l'utilisateur décodées à l'objet de la requête
+        next(); // Passer au middleware suivant
+    });
+};
+
 // Route pour récupérer tous les utilisateurs
-// Cette route répond aux requêtes GET à l'URL '/users'
 app.get('/users', (req, res) => {
-    // Renvoie la liste des utilisateurs sous forme de JSON
-    res.json(users);
+    res.json(users); // Renvoyer la liste des utilisateurs sous forme de JSON
 });
 
 // Route pour récupérer un utilisateur par ID
-// Cette route répond aux requêtes GET à l'URL '/users/id'
 app.get('/users/:id', (req, res) => {
-    // Extraire l'ID de l'utilisateur à partir des paramètres de l'URL
-    const userId = parseInt(req.params.id, 10);
-    // Trouver l'utilisateur avec l'ID correspondant
-    const user = users.find(u => u.id === userId);
+    const userId = parseInt(req.params.id, 10); // Extraire l'ID de l'utilisateur à partir des paramètres de l'URL
+    const user = users.find(u => u.id === userId); // Trouver l'utilisateur avec l'ID correspondant
     if (user) {
-        // Si l'utilisateur est trouvé, renvoyer les détails de l'utilisateur en JSON
-        res.json(user);
+        res.json(user); // Si trouvé, renvoyer les détails de l'utilisateur
     } else {
-        // Si l'utilisateur n'est pas trouvé, renvoyer une réponse 404 avec un message d'erreur
-        res.status(404).send('Utilisateur non trouvé');
+        res.status(404).send('Utilisateur non trouvé'); // Si non trouvé, renvoyer une réponse 404
     }
 });
 
 // Route pour créer un nouvel utilisateur
-// Cette route répond aux requêtes POST à l'URL '/users'
-app.post('/users', (req, res) => {
-    // Extraire le nom et l'email du corps de la requête
-    const { name, email } = req.body;
-    // Vérifier que le nom et l'email sont fournis
-    if (!name || !email) {
-        // Si l'un des champs est manquant, renvoyer une réponse 400 avec un message d'erreur
-        return res.status(400).send('Name and email are required');
+app.post('/users', hashPassword, (req, res) => {
+    const { name, email, password } = req.body; // Extraire les informations du corps de la requête
+    if (!name || !email || !password) {
+        return res.status(400).send('Name, email and password are required'); // Si des champs sont manquants, renvoyer une réponse 400
     }
 
-    // Créer un nouvel utilisateur avec un ID unique
     const newUser = {
         id: users.length + 1,
         name: name,
-        email: email
+        email: email,
+        password: password
     };
-    // Ajouter le nouvel utilisateur au tableau des utilisateurs
-    users.push(newUser);
-    // Renvoie une réponse 201 (Created) avec les détails du nouvel utilisateur
-    res.status(201).json(newUser);
+    users.push(newUser); // Ajouter le nouvel utilisateur au tableau des utilisateurs
+    res.status(201).json(newUser); // Renvoyer une réponse 201 avec les détails du nouvel utilisateur
 });
 
 // Route pour mettre à jour un utilisateur existant
-// Cette route répond aux requêtes PUT à l'URL '/users/id'
-app.put('/users/:id', (req, res) => {
-    // Extraire l'ID de l'utilisateur à partir des paramètres de l'URL
-    const userId = parseInt(req.params.id, 10);
-    // Trouver l'index de l'utilisateur avec l'ID correspondant
-    const userIndex = users.findIndex(u => u.id === userId);
+app.put('/users/:id', authenticateToken, (req, res) => {
+    const userId = parseInt(req.params.id, 10); // Extraire l'ID de l'utilisateur à partir des paramètres de l'URL
+    const userIndex = users.findIndex(u => u.id === userId); // Trouver l'index de l'utilisateur avec l'ID correspondant
 
     if (userIndex !== -1) {
-        // Extraire le nom et l'email du corps de la requête
-        const { name, email } = req.body;
-        // Vérifier que le nom et l'email sont fournis
-        if (!name || !email) {
-            // Si l'un des champs est manquant, renvoyer une réponse 400 avec un message d'erreur
-            return res.status(400).send('Name and email are required');
+        const { name, email, password } = req.body; // Extraire les informations du corps de la requête
+        if (!name || !email || !password) {
+            return res.status(400).send('Name, email and password are required'); // Si des champs sont manquants, renvoyer une réponse 400
         }
 
-        // Mettre à jour les détails de l'utilisateur
         users[userIndex] = {
             id: userId,
             name: name,
-            email: email
+            email: email,
+            password: password
         };
-        // Renvoyer les détails mis à jour de l'utilisateur
-        res.json(users[userIndex]);
+        res.json(users[userIndex]); // Renvoyer les détails mis à jour de l'utilisateur
     } else {
-        // Si l'utilisateur n'est pas trouvé, renvoyer une réponse 404 avec un message d'erreur
-        res.status(404).send('Utilisateur non trouvé');
+        res.status(404).send('Utilisateur non trouvé'); // Si l'utilisateur n'est pas trouvé, renvoyer une réponse 404
     }
 });
 
 // Route pour supprimer un utilisateur
-// Cette route répond aux requêtes DELETE à l'URL '/users/:id'
 app.delete('/users/:id', (req, res) => {
-    // Extraire l'ID de l'utilisateur à partir des paramètres de l'URL
-    const userId = parseInt(req.params.id, 10);
-    // Trouver l'index de l'utilisateur avec l'ID correspondant
-    const userIndex = users.findIndex(u => u.id === userId);
+    const userId = parseInt(req.params.id, 10); // Extraire l'ID de l'utilisateur à partir des paramètres de l'URL
+    const userIndex = users.findIndex(u => u.id === userId); // Trouver l'index de l'utilisateur avec l'ID correspondant
 
     if (userIndex !== -1) {
-        // Supprimer l'utilisateur du tableau
-        const deletedUser = users.splice(userIndex, 1);
-        // Renvoie les détails de l'utilisateur supprimé
-        res.json(deletedUser);
+        const deletedUser = users.splice(userIndex, 1); // Supprimer l'utilisateur du tableau
+        res.json(deletedUser); // Renvoyer les détails de l'utilisateur supprimé
     } else {
-        // Si l'utilisateur n'est pas trouvé, renvoyer une réponse 404 avec un message d'erreur
-        res.status(404).send('Utilisateur non trouvé');
+        res.status(404).send('Utilisateur non trouvé'); // Si l'utilisateur n'est pas trouvé, renvoyer une réponse 404
     }
 });
 
-// Définir le port sur lequel le serveur écoutera
+// Définition du port sur lequel le serveur écoutera
 const PORT = 3000;
 
 // Démarrer le serveur et écouter sur le port spécifié
 app.listen(PORT, () => {
-    // Afficher un message dans la console lorsque le serveur démarre
-    console.log(`Le serveur tourne sur le port ${PORT}`);
+    console.log(`Le serveur tourne sur le port ${PORT}`); // Afficher un message dans la console lorsque le serveur démarre
 });
